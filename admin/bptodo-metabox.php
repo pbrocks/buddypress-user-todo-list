6bbbb14a6c7492ac1af1fd38119b720fb1dfa97c
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2016-11-28 16:15:32 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- admin/bptodo-metabox.php
+++ admin/bptodo-metabox.php
@@ -1,27 +1,27 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
 
 //Metabox to show the due date of todo item
-add_action('add_meta_boxes','bptodo_due_date_metabox');
+add_action('add_meta_boxes', 'bptodo_due_date_metabox');
 function bptodo_due_date_metabox() {
-	add_meta_box( 'bptodo-metabox', 'Todo Due Date', 'bptodo_due_date_content', 'bp-todo', 'side', 'high', null );
+	add_meta_box('bptodo-metabox', 'Todo Due Date', 'bptodo_due_date_content', 'bp-todo', 'side', 'high', null);
 }
 
 function bptodo_due_date_content() {
 	global $post;
-	$due_date = get_post_meta( $post->ID, 'todo_due_date', true );
+	$due_date = get_post_meta($post->ID, 'todo_due_date', true);
 	?>
-	<input type="date" name="todo_due_date" required value="<?php echo $due_date;;?>">
+	<input type="date" name="todo_due_date" required value="<?php echo $due_date; ;?>">
 	<?php
 }
 
 //Save due date on save post
-add_action( 'save_post', 'bptodo_save_due_date' );
-function bptodo_save_due_date( $post_id ) {
-	$post = get_post( $post_id );
-	if( $post->post_type == 'bp-todo' ) {
-		if( isset( $_POST['todo_due_date'] ) ) {
-			update_post_meta( $post_id, 'todo_due_date', $_POST['todo_due_date'] );
+add_action('save_post', 'bptodo_save_due_date');
+function bptodo_save_due_date($post_id) {
+	$post = get_post($post_id);
+	if ($post->post_type == 'bp-todo') {
+		if (isset($_POST['todo_due_date'])) {
+			update_post_meta($post_id, 'todo_due_date', $_POST['todo_due_date']);
 		}
 	}
 }
--- inc/bptodo-scripts.php
+++ inc/bptodo-scripts.php
@@ -1,14 +1,14 @@
 <?php
 // Exit if accessed directly
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 //Class to add custom scripts and styles
-if( !class_exists( 'BP_Todo_Scripts_Styles' ) ) {
-	class BP_Todo_Scripts_Styles{
+if (!class_exists('BP_Todo_Scripts_Styles')) {
+	class BP_Todo_Scripts_Styles {
 
 		//constructor
 		function __construct() {
-			add_action( 'wp_enqueue_scripts', array( $this, 'bptodo_custom_variables' ) );
+			add_action('wp_enqueue_scripts', array($this, 'bptodo_custom_variables'));
 		}
 
 		/**
@@ -16,10 +16,10 @@ if( !class_exists( 'BP_Todo_Scripts_Styles' ) ) {
 		 */
 		function bptodo_custom_variables() {
 			$curr_url = $_SERVER['REQUEST_URI'];
-			if( strpos( $curr_url, 'todo' ) !== false ) {
-				wp_enqueue_script('bptodo-js-date','//cdn.jsdelivr.net/webshim/1.14.5/polyfiller.js', array('jquery'));
-				wp_enqueue_script('bptodo-js-front',BPTODO_PLUGIN_URL.'assets/js/bptodo-front.js', array('jquery'));
-				wp_localize_script( 'bptodo-js-front', 'bptodo_ajax_object',array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
+			if (strpos($curr_url, 'todo') !== false) {
+				wp_enqueue_script('bptodo-js-date', '//cdn.jsdelivr.net/webshim/1.14.5/polyfiller.js', array('jquery'));
+				wp_enqueue_script('bptodo-js-front', BPTODO_PLUGIN_URL.'assets/js/bptodo-front.js', array('jquery'));
+				wp_localize_script('bptodo-js-front', 'bptodo_ajax_object', array('ajax_url' => admin_url('admin-ajax.php')));
 				wp_enqueue_style('bptodo-front-css', BPTODO_PLUGIN_URL.'assets/css/bptodo-front.css');
 			}
 		}
