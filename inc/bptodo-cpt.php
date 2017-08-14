58c0ebc0623f793393767b14ea3a00089f6fac09
Pallavi Gupta <pallavi@wbcomdesigns.com>
2017-08-14 13:30:34 +0530

    #13 fixed rewrite rule for sinple todo view
    #13 fixed rewrite rules for sinple todo view.

--- inc/bptodo-cpt.php
+++ inc/bptodo-cpt.php
@@ -1,82 +1,86 @@
 <?php
+
 // Exit if accessed directly
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
 //Class to add admin menu to manage general settings
-if (!class_exists('BP_Todo_CPT')) {
+if ( !class_exists( 'BP_Todo_CPT' ) ) {
+
 	class BP_Todo_CPT {
 
 		//constructor
 		function __construct() {
-			add_action('init', array($this, 'bptodo_create_cpt'));
-			add_action('init', array($this, 'bptodo_create_cpt_category'));
+			add_action( 'init', array( $this, 'bptodo_create_cpt' ) );
+			add_action( 'init', array( $this, 'bptodo_create_cpt_category' ) );
 		}
 
 		//Actions performed on loading init: creating cpt
 		function bptodo_create_cpt() {
 			$labels = array(
-				'name' => 'Todo Items',
-				'singular_name' => 'Todo Item',
-				'menu_name' => 'Todo Items',
-				'name_admin_bar' => 'Todo Items',
-				'view_item' => 'View Todo Item',
-				'all_items' => 'All Todo Items',
-				'search_items' => 'Search Todo Item',
-				'parent_item_colon' => 'Parent Todo Item:',
-				'not_found' => 'No Todo Item Found',
+				'name'				 => 'Todo Items',
+				'singular_name'		 => 'Todo Item',
+				'menu_name'			 => 'Todo Items',
+				'name_admin_bar'	 => 'Todo Items',
+				'view_item'			 => 'View Todo Item',
+				'all_items'			 => 'All Todo Items',
+				'search_items'		 => 'Search Todo Item',
+				'parent_item_colon'	 => 'Parent Todo Item:',
+				'not_found'			 => 'No Todo Item Found',
 				'not_found_in_trash' => 'No Todo Item Found In Trash',
 			);
-			
+
 			$args = array(
-				'labels' => $labels,
-				'public' => true,
-				'menu_icon' => 'dashicons-edit',
+				'labels'			 => $labels,
+				'public'			 => true,
+				'menu_icon'			 => 'dashicons-edit',
 				'publicly_queryable' => true,
-				'show_ui' => true,
-				'show_in_menu' => true,
-				'query_var' => true,
-				'rewrite' => array('slug' => 'todo', 'with_front' => false),
-				'capability_type' => 'post',
-				'capabilities' => array(
-					'create_posts' => false,
-					'delete_posts' => false,
-					'edit_post' => false,
+				'show_ui'			 => true,
+				'show_in_menu'		 => true,
+				'query_var'			 => true,
+				'rewrite'			 => array( 'slug' => 'todo', 'with_front' => false ),
+				'capability_type'	 => 'post',
+				'capabilities'		 => array(
+					'create_posts'	 => false,
+					'delete_posts'	 => false,
+					'edit_post'		 => false,
 				),
-				'has_archive' => true,
-				'hierarchical' => false,
-				'menu_position' => null,
-				'supports' => array('title', 'editor', 'author', 'thumbnail'),
+				'has_archive'		 => true,
+				'hierarchical'		 => false,
+				'menu_position'		 => null,
+				'supports'			 => array( 'title', 'editor', 'author', 'thumbnail' ),
 			);
-			register_post_type('bp-todo', $args);
+			register_post_type( 'bp-todo', $args );
+			flush_rewrite_rules( FALSE );
 		}
 
 		//Actions performed on loading init: creating cpt category
 		function bptodo_create_cpt_category() {
-			$tax_labels = array(
-				'name' => 'Category',
-				'singular_name' => 'Category',
-				'search_items' => 'Search Todo Items Categories',
-				'all_items' => 'All Todo Items Categories',
-				'parent_item' => 'Parent Todo Item Category',
-				'parent_item_colon' => 'Parent Todo Item Category:',
-				'edit_item' => 'Edit Category',
-				'update_item' => 'Update Category',
-				'add_new_item' => 'Add Todo Item Category',
-				'not_found' => 'No Todo Item Categories Found',
-				'menu_name' => 'Category',
-				
+			$tax_labels	 = array(
+				'name'				 => 'Category',
+				'singular_name'		 => 'Category',
+				'search_items'		 => 'Search Todo Items Categories',
+				'all_items'			 => 'All Todo Items Categories',
+				'parent_item'		 => 'Parent Todo Item Category',
+				'parent_item_colon'	 => 'Parent Todo Item Category:',
+				'edit_item'			 => 'Edit Category',
+				'update_item'		 => 'Update Category',
+				'add_new_item'		 => 'Add Todo Item Category',
+				'not_found'			 => 'No Todo Item Categories Found',
+				'menu_name'			 => 'Category',
 			);
-			$tax_args = array(
-				'hierarchical' => true,
-				'labels' => $tax_labels,
-				'show_ui' => true,
-				'show_admin_column' => true,
-				'public' => true,
-				'query_var' => true,
-				'rewrite' => array('slug' => 'todo_category'),
+			$tax_args	 = array(
+				'hierarchical'		 => true,
+				'labels'			 => $tax_labels,
+				'show_ui'			 => true,
+				'show_admin_column'	 => true,
+				'public'			 => true,
+				'query_var'			 => true,
+				'rewrite'			 => array( 'slug' => 'todo_category' ),
 			);
-			register_taxonomy('todo_category', array('bp-todo'), $tax_args);
+			register_taxonomy( 'todo_category', array( 'bp-todo' ), $tax_args );
 		}
+
 	}
+
 	new BP_Todo_CPT();
 }
